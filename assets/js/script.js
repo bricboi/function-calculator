dbc1f1d86de93ed42f1adc620b937d336d8544e7
bricboi <>
2023-09-02 15:31:50 +0000

    Add functionpad and basic calculation function and output display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 let inputArray = [];
+let fValue = "";
+let result = 0;
 
+// numberPad function for when buttons are pressed /* remove console.log outputs!!! */
 function numberPad(value) {
     switch (value) {
         case "1":
@@ -65,10 +68,71 @@ function numberPad(value) {
         case "clear":
             console.log("Clear All");
             inputArray = [];
+            result = "";
+            let outputField = document.getElementById("output-field");
+            outputField.textContent = result;
             console.log(inputArray);
+            console.log(result);
             break;
     }
     let inputField = document.getElementById("input-field");
-
     inputField.textContent = inputArray.toString().replaceAll(",", "");
+}
+
+// functionPad function for when buttons are pressed /* remove console.log outputs!!! */
+function functionPad(value) {
+    switch (value) {
+        case "sin":
+            console.log("Sin(x)");
+            fValue = "Sin";
+            calculateSin();
+            break;
+        case "cos":
+            console.log("Cos(x)");
+            fValue = "Cos";
+            break;
+        case "tan":
+            console.log("Tan(x)");
+            fValue = "Tan";
+            break;
+        case "asin":
+            console.log("arc Sin(x)");
+            fValue = "aSin";
+            break;
+        case "acos":
+            console.log("arc Cos(x)");
+            fValue = "aCos";
+            break;
+        case "atan":
+            console.log("arc Tan(x)");
+            fValue = "aTan";
+            break;
+        case "log":
+            console.log("Log(x)");
+            fValue = "Log";
+            break;
+        case "ln":
+            console.log("Ln");
+            fValue = "Ln";
+            break;
+        case "e":
+            console.log("e^x");
+            fValue = "eX";
+            break;
+        case "=":
+            console.log("=");
+            fValue = "=";
+            let outputField = document.getElementById("output-field");
+            outputField.textContent = result;
+            break;
+    }
+}
+
+// Calculation function for Sin(x) /* remove console.log outputs!!! */
+function calculateSin() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.sin(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `Sin (${x})`;
 }
