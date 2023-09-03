027d70c194054ce42c57a6b48cc64bbdcefa34f1
bricboi <>
2023-09-03 11:41:31 +0000

    Add more calculation functions and make buttons execute them

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ let inputArray = [];
 let fValue = "";
 let result = 0;
 
+// remember to remove console.log outputs before submitting!!!
+
 // numberPad function for when buttons are pressed /* remove console.log outputs!!! */
 function numberPad(value) {
     switch (value) {
@@ -90,34 +92,42 @@ function functionPad(value) {
         case "cos":
             console.log("Cos(x)");
             fValue = "Cos";
+            calculateCos();
             break;
         case "tan":
             console.log("Tan(x)");
             fValue = "Tan";
+            calculateTan();
             break;
         case "asin":
             console.log("arc Sin(x)");
             fValue = "aSin";
+            calculateArcSin();
             break;
         case "acos":
             console.log("arc Cos(x)");
             fValue = "aCos";
+            calculateArcCos();
             break;
         case "atan":
             console.log("arc Tan(x)");
             fValue = "aTan";
+            calculateArcTan();
             break;
         case "log":
             console.log("Log(x)");
             fValue = "Log";
+            calculateLog10();
             break;
         case "ln":
             console.log("Ln");
             fValue = "Ln";
+            calculateLn();
             break;
         case "e":
             console.log("e^x");
             fValue = "eX";
+            calculateExponent();
             break;
         case "=":
             console.log("=");
@@ -128,11 +138,79 @@ function functionPad(value) {
     }
 }
 
-// Calculation function for Sin(x) /* remove console.log outputs!!! */
+// Calculation functions below vvv
+
+// Sin, cos and tan /* remove console.log outputs!!! */
 function calculateSin() {
     let x = parseFloat(inputArray.join(''));
     result = Math.sin(x);
     console.log(result);
     let inputField = document.getElementById("input-field");
-    inputField.textContent = `Sin (${x})`;
+    inputField.textContent = `Sin(${x})`;
+}
+
+function calculateCos() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.cos(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `Cos(${x})`;
+}
+
+function calculateTan() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.tan(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `Tan(${x})`;
+}
+
+// arc functions
+function calculateArcSin() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.asin(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `arc Sin(${x})`;
+}
+
+function calculateArcCos() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.acos(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `arc Cos(${x})`;
+}
+
+function calculateArcTan() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.asin(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `arc Tan(${x})`;
+}
+
+// log and e functions
+function calculateLog10() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.log10(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `Log10(${x})`;
+}
+
+function calculateLn() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.log(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `Log(${x})`;
+}
+
+function calculateExponent() {
+    let x = parseFloat(inputArray.join(''));
+    result = Math.exp(x);
+    console.log(result);
+    let inputField = document.getElementById("input-field");
+    inputField.textContent = `e^(${x})`;
 }
