50518865a5d9f9899eac830ceb7a34f26fd6c5bf
bricboi <>
2023-09-05 09:41:45 +0000

    Add CSS to change scaling depending on screen size

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,104 +1,50 @@
-// const funcDiv = document.getElementsByClassName("function-div");
-
 let inputArray = [];
 let result = 0;
 
-/* Check JS and try to make buttons from JS,
-check if there are things to remove or add.
-*/
-
-// function numLoad() {
-//     let numDiv = document.getElementsByClassName("test");
-//     numDiv.innerHTML = `
-//     <button value="1" onclick="numberPad(value)">1</button>
-//     <button value="2" onclick="numberPad(value)">2</button>
-//     <button value="3" onclick="numberPad(value)">3</button>
-//     <button value="4" onclick="numberPad(value)">4</button>
-//     <button value="5" onclick="numberPad(value)">5</button>
-//     <button value="6" onclick="numberPad(value)">6</button>
-//     <button value="7" onclick="numberPad(value)">7</button>
-//     <button value="8" onclick="numberPad(value)">8</button>
-//     <button value="9" onclick="numberPad(value)">9</button>
-//     <button value="0" onclick="numberPad(value)">0</button>
-//     <button value="." onclick="numberPad(value)">.</button>
-//     <button value="del" onclick="numberPad(value)">del</button>
-//     <button value="clear" onclick="numberPad(value)">Clear all</button>
-// `;
-// }
-
-// remember to remove console.log outputs before submitting!!!
-// 20 is max input of numbers
-// numberPad function for when buttons are pressed /* remove console.log outputs!!! */
+// numberPad function for when buttons are pressed
 function numberPad(value) {
     switch (value) {
         case "1":
-            console.log("1");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "2":
-            console.log("2");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "3":
-            console.log("3");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "4":
-            console.log("4");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "5":
-            console.log("5");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "6":
-            console.log("6");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "7":
-            console.log("7");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "8":
-            console.log("8");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "9":
-            console.log("9");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "0":
-            console.log("0");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case ".":
-            console.log(".");
             inputArray.push(value);
-            console.log(inputArray);
             break;
         case "del":
-            console.log("Delete");
             inputArray.pop();
-            console.log(inputArray);
             break;
         case "clear":
-            console.log("Clear All");
             inputArray = [];
             result = "";
             let outputField = document.getElementById("output-field");
             outputField.textContent = result;
-            console.log(inputArray);
-            console.log(result);
             break;
     }
     // Resets output field and updates the input field
@@ -109,43 +55,34 @@ function numberPad(value) {
     inputField.textContent = inputArray.toString().replaceAll(",", "");
 }
 
-// functionPad function for when buttons are pressed /* remove console.log outputs!!! */
+// functionPad function for when buttons are pressed
 function functionPad(value) {
     switch (value) {
         case "sin":
-            console.log("Sin(x)");
             calculateSin();
             break;
         case "cos":
-            console.log("Cos(x)");
             calculateCos();
             break;
         case "tan":
-            console.log("Tan(x)");
             calculateTan();
             break;
         case "asin":
-            console.log("arc Sin(x)");
             calculateArcSin();
             break;
         case "acos":
-            console.log("arc Cos(x)");
             calculateArcCos();
             break;
         case "atan":
-            console.log("arc Tan(x)");
             calculateArcTan();
             break;
         case "log":
-            console.log("Log(x)");
             calculateLog10();
             break;
         case "ln":
-            console.log("Ln");
             calculateLn();
             break;
         case "e":
-            console.log("e^x");
             calculateExponent();
             break;
     }
@@ -155,16 +92,14 @@ function functionPad(value) {
 // Calculation functions below vvv
 // Final calculation
 function finalCalculation() {
-    console.log("=");
     let outputField = document.getElementById("output-field");
     outputField.textContent = ` = ${result}`;
 }
 
-// Sin, cos and tan /* remove console.log outputs!!! */
+// Sin, cos and tan
 function calculateSin() {
     let x = parseFloat(inputArray.join(''));
     result = Math.sin(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `Sin(${x})`;
 }
@@ -172,7 +107,6 @@ function calculateSin() {
 function calculateCos() {
     let x = parseFloat(inputArray.join(''));
     result = Math.cos(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `Cos(${x})`;
 }
@@ -180,7 +114,6 @@ function calculateCos() {
 function calculateTan() {
     let x = parseFloat(inputArray.join(''));
     result = Math.tan(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `Tan(${x})`;
 }
@@ -189,7 +122,6 @@ function calculateTan() {
 function calculateArcSin() {
     let x = parseFloat(inputArray.join(''));
     result = Math.asin(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `arc Sin(${x})`;
 }
@@ -197,7 +129,6 @@ function calculateArcSin() {
 function calculateArcCos() {
     let x = parseFloat(inputArray.join(''));
     result = Math.acos(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `arc Cos(${x})`;
 }
@@ -205,7 +136,6 @@ function calculateArcCos() {
 function calculateArcTan() {
     let x = parseFloat(inputArray.join(''));
     result = Math.asin(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `arc Tan(${x})`;
 }
@@ -214,7 +144,6 @@ function calculateArcTan() {
 function calculateLog10() {
     let x = parseFloat(inputArray.join(''));
     result = Math.log10(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `Log10(${x})`;
 }
@@ -222,7 +151,6 @@ function calculateLog10() {
 function calculateLn() {
     let x = parseFloat(inputArray.join(''));
     result = Math.log(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `Log(${x})`;
 }
@@ -230,7 +158,6 @@ function calculateLn() {
 function calculateExponent() {
     let x = parseFloat(inputArray.join(''));
     result = Math.exp(x);
-    console.log(result);
     let inputField = document.getElementById("input-field");
     inputField.textContent = `e^(${x})`;
 }
